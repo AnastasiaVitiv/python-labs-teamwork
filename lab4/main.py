d68e4b864c5b728964c4d478983f8ec0c7c7b63d
Vitiv Anastasia <vitiv.tuska@gmail.com>
2024-11-18 17:15:07 +0200

    changed ship name

--- lab4/main.py
+++ lab4/main.py
@@ -1,13 +1,12 @@
 from ship import Ship
-
 def main():
-    ship1 = Ship(15555, "Leonard", 555, "Lui", 55, 2444)
-    ship2 = Ship(27777, "Di", 777, "Devid", 77, 455)
-    ship3 = Ship(32222, "Caprio", 222, "Lui", 22, 99)
+    leonard_ship = Ship(15555, "Leonard", 555, "Lui", 55, 2444)
+    di_ship = Ship(27777, "Di", 777, "Devid", 77, 455)
+    caprio_ship = Ship(32222, "Caprio", 222, "Lui", 22, 99)
 
-    print(ship1)
-    print(ship2)
-    print(ship3)
+    print(leonard_ship)
+    print(di_ship)
+    print(caprio_ship)
 
 if __name__ == "__main__":
     main()
